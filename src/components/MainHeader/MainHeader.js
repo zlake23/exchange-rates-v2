f582bd08079cc994ad2fdcb55d2e12fffb54d7c4
Zachary Lake <zlake23@gmail.com>
2020-06-02 19:26:10 -0700

    Updates code for header component

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import './MainHeader.css';
 
 function MainHeader(props) {
-  const [selectedBase, setSelectedBase] = useState('EUR');
+  
   return (
     <header>
     <h1>{props.title}</h1>
-    <h2>Base Currency: 1 {selectedBase}</h2>
+    <h2>{props.subtitle}</h2>
 </header>
   );
 }
