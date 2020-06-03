de35b61d0f5b53fa0b951d6be5ebd87cf168ee49
Zachary Lake <zlake23@gmail.com>
2020-06-02 17:44:47 -0700

    Updates to app.js code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [selectedRegionTitle, setSelectedRegionTitle] = useState('Europe');
+  const [selectedBase, setSelectedBase] = useState('EUR');
+  const selectedRegion = {
+    'Europe': ['BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK', 'HRK', 'RUB'],
+    'Asia': ['CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MYR', 'PHP', 'SGD', 'THB', 'TRY'],
+    'North America': ['USD', 'CAD', 'MXN'],
+    'South America': ['BRL'],
+    'Africa/Oceania': ['ZAR', 'AUD', 'NZD'],
+  };
+  let [rates, setRates] = useState([])
+
+  function doFetch() {
+    fetch('https://api.exchangeratesapi.io/latest?base=' + selectedBase)
+        .then(response => response.json())
+        .then(data => {
+          setRates(Object.entries(data.rates));
+        });
+  }
+
+useEffect(doFetch, [selectedRegionTitle, selectedBase]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+            <h1>Currency Exchange Rates</h1>
+            <h2>Base Currency: 1 {selectedBase}</h2>
+        </header>
+
+        <div className="Container">
+            <div className="Graph">
+                <div className="Graph-select">
+                  <div>
+                    <span>Select Continental Region:</span>
+                    <select onChange={event => setSelectedRegionTitle(event.target.value)}>
+                        
+                        <option>Europe</option>
+                        <option>Asia</option>
+                        <option>North America</option>
+                        <option>South America</option>
+                        <option>Africa/Oceania</option>
+                    </select>
+                    </div>
+                    <div>
+                    <span>Select base currency:</span>
+                    <select onChange={event => setSelectedBase(event.target.value)}>
+                        <option>EUR</option>
+                        <option>USD</option>
+                        <option>CAD</option>
+                        <option>GBP</option>
+                        <option>AUD</option>
+                    </select>
+                    </div>
+                </div>
+                <div className="Graph-text">
+                  {
+                    rates
+                    .sort()
+                    .filter(rate => selectedRegion[selectedRegionTitle].includes(rate[0]))
+                    .map(rate => (
+                    <div className="Graph-text" key={rate[0]}>
+                      {rate[0]}, {rate[1]}
+                    </div>
+                    ))
+                  }
+                </div>
+                <div className="Graph-content">
+                  {
+                    rates
+                    .filter(rate => selectedRegion[selectedRegionTitle].includes(rate[0]))
+                    .map(rate => (
+                    <div className="Graph-bar" key={rate[0]} style={{width: (1/rate[1] * 100) + '%'}}></div>  
+                    ))
+                  }
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
